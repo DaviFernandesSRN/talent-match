1232fa0155001dc19f2a18358b2267def58e07a7
Davi <davifernandessrn@gmail.com>
2026-01-22 13:14:05 -0300

    Configurando Vite para evitar React duplicado

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // ESTA É A CURA: Força todo mundo a usar o mesmo React
+    dedupe: ['react', 'react-dom'], 
+  },
 })
